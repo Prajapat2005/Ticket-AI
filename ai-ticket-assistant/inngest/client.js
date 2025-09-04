5720bd08ef9698d79c94b9e2edcebb845ac5c880
Prajapat2005 <parthprajapat2005@gmail.com>
2025-09-05 01:29:47 +0530

    initial commit

--- ai-ticket-assistant/inngest/client.js
+++ ai-ticket-assistant/inngest/client.js
@@ -1,5 +1,8 @@
 import { Inngest } from "inngest";
+import dotenv from "dotenv"
+dotenv.config()
 
 export const inngest = new Inngest({
     id: "ticketing-system",
+    signingKey: process.env.INNGEST_SIGNKEY_KEY
 });
